cccb1048f8f9c1569ba6cdeb7d8457a8197bd6c3
rober-carrillo <robercarrillog@gmail.com>
2025-05-30 20:13:18 -0400

    tested integration with arduino IDE

--- EnduranceClock.ino
+++ EnduranceClock.ino
@@ -11,7 +11,7 @@ const int ledPin = 11;
 void setup() {
   myservo.attach(SERVO_PIN);            // attaches the servo on pin 9 to the servo object
   myservo.write(pos);                   // go to position zero first
-  delay(2000);                          // wait for some time
+  delay(1000);                          // wait for some time
 
 
 
@@ -22,12 +22,12 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);                          // delay of 1 second before start
+  delay(400);                          // delay of 1 second before start
   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees in steps of 1 degree
     myservo.write(pos);                 // tell servo to go to position in variable 'pos'
     delay(15);                          // waits 15 ms for the servo to reach the position
   }
-  delay(1000);                          // delay of 1 second before changing direction of rotation
+  delay(200);                          // delay of 1 second before changing direction of rotation
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);                 // tell servo to go to position in variable 'pos'
     delay(15);                          // waits 15 ms for the servo to reach the position
